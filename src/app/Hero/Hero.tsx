553c3169334b0db5b74d0a9e07051a9a28ac2bb4
Varun1452 <varungaudani80@gmail.com>
2024-12-19 20:11:32 +0530

    hero section completed

--- src/app/Hero/Hero.tsx
+++ src/app/Hero/Hero.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable @next/next/no-img-element */
-/* eslint-disable jsx-a11y/alt-text */
 "use client";
 import React from "react";
 
@@ -11,9 +9,21 @@ const Hero = () => {
           Hello! I Am <span className="text-[#7127BA]">Varun Gaudani</span>
         </span>
       </div>
+      <div className=" z-50 absolute ml-8 md:ml-[650px] mt-40 md:mt-[240px]">
+        <span className="text-6xl text-white mt-36">
+          Judge a book <br /> by its
+          <span className="text-[#7127BA]">Cover</span>
+        </span>{" "}
+        <p className="text-white text-xl mt-[10px]">
+          Because if the cover does not impress you what else can?
+        </p>
+      </div>
       <div className=" ml-0 items-center flex justify-center md:ml-[350px] w-full  md:w-[250px] relative ">
         <div className=" pt-44 ">
-          <img src="./Me.png" className="rounded-full animate-pulse " />
+          <img
+            src="./Me.png"
+            className="rounded-full md:animate-pulse opacity-50 "
+          />
         </div>
         <img
           src="./Arrow.png"
