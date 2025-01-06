b6d1d57255c47dd430be0f008d6164abd53a1876
Varun1452 <varungaudani80@gmail.com>
2025-01-06 13:57:11 +0530

    home butoon add on VARUN in header section

--- src/app/Hero/Hero.tsx
+++ src/app/Hero/Hero.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className=" ">
+    <div className=" " id="hero">
       <div className="  absolute flex ml-8 md:ml-[550px] mt-[85px]  text-xl ">
         <p>Hello 👋 I’m</p>
         <section className="animation">
