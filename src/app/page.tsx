f87c5cdfacc273e9a8da7a72baea7a2972410504
Varun1452 <varungaudani80@gmail.com>
2025-01-08 14:19:46 +0530

    animation added to all sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 "use client";
-import { useEffect } from "react";
 import { Preahvihear } from "next/font/google";
-import AOS from "aos";
 import "aos/dist/aos.css"; // Import AOS styles
 import Header from "./Header/Header";
 import Hero from "./Hero/Hero";
@@ -17,15 +15,6 @@ const preahvihear = Preahvihear({
 });
 
 export default function Home() {
-  useEffect(() => {
-    AOS.init({
-      duration: 1000, // Animation duration
-      offset: 50, // Offset to trigger animation
-      easing: "ease-in-out", // Animation easing
-      once: true, // Whether animation should happen only once
-    });
-  }, []);
-
   return (
     <div className={`${preahvihear.className}`}>
       <Header />
