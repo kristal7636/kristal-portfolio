a30aa96c2215f963fa336e6334da121426d7eaf2
Varun1452 <varungaudani80@gmail.com>
2025-02-21 14:56:36 +0530

    removed unused useeffects

--- src/app/ContactMe/ContactMe.tsx
+++ src/app/ContactMe/ContactMe.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 
 // Define the types for form data and error messages
 type FormData = {
--- src/app/Skills/Skills.tsx
+++ src/app/Skills/Skills.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect } from "react";
+import React from "react";
 
 const skillsData = [
   { src: "./html5.svg", alt: "HTML5 Logo", label: "HTML5" },
